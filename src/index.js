bbb310c4b553aa7757aa188ba8bc5e8d62bb2f20
Christian <christianyip92@gmail.com>
2022-08-24 16:42:29 -0700

    Add basic modal.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 console.log('Testing index.js');
 import { createTodoItem } from "./todo-item";
 createTodoItem();
+
+const modal = document.getElementById("task-modal");
+const taskBtn = document.querySelectorAll("#add-task-btn");
+const span = document.getElementsByClassName("close")[0];
+
+taskBtn.forEach(btn => {
+    btn.onclick = function() {
+        modal.style.display = "block";
+      }
+})
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
