01183ea4c3e81dd6ab84470696bc11c5fd8a0081
Christian <christianyip92@gmail.com>
2022-08-25 13:02:15 -0700

    Add placeholder content for task item.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 console.log('Testing index.js');
-import { createTodoItem } from "./todo-item";
+// import { createTodoItem } from "./todo-item";
 import { modalControl } from "./modal-control";
-createTodoItem();
+// createTodoItem();
 modalControl();
