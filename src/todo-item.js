cd77fb8d30a023a4892bf3353532d3d871e0585a
Christian <christianyip92@gmail.com>
2022-08-25 12:47:23 -0700

    Add styling for content heading.

--- src/todo-item.js
+++ src/todo-item.js
@@ -1,5 +1,14 @@
-export function createTodoItem() {
-    const taskContainer = document.querySelector('.task-container');
-    const newTask = document.createElement('li');
-    taskContainer.appendChild(newTask);
+export class TodoItem {
+    constructor() {
+        this.title;
+        this.priority;
+        this.description;
+        this.date;
+    }
+
+    createTodoItem() {
+        const taskContainer = document.querySelector('.task-container');
+        const newTask = document.createElement('li');
+        taskContainer.appendChild(newTask);
+    }
 }
