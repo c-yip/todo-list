a584f5e9894604750b287641d1f6741e4bc9ceb4
Christian <christianyip92@gmail.com>
2022-08-26 13:18:27 -0700

    Add function and icon that removes task item from array.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 console.log('Testing index.js');
-// import { createTodoItem } from "./todo-item";
 import { modalControl } from "./modal-control";
-// createTodoItem();
+import { removeItem } from "./content-control";
 modalControl();
+removeItem();
