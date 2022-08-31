848e84160e33c3d6a8ecd4bc3dc5a864b2fd9746
Christian <christianyip92@gmail.com>
2022-08-31 15:35:02 -0700

    Fix issue with app breaking on load.
    Last commit did not fix.

--- src/create-todo-item.js
+++ src/create-todo-item.js
@@ -2,6 +2,7 @@ export let todoArray = [];
 import { createDom } from "./content-control";
 const projectArray = [];
 let id = null;
+let firstLoad = true;
 
 export class TodoItem {
     constructor(title, priority, description, date, project) {
@@ -30,29 +31,33 @@ export function createTodoItem() {
         projectArray.push(newItem);
     }
     console.log(projectArray);
+    firstLoad = false;
+    console.log(firstLoad);
 
     // local storage
     localStorage.setItem('todoArrayStorage', JSON.stringify(todoArray));
     localStorage.setItem('idCount', JSON.stringify(id));
+    localStorage.setItem('firstLoad', JSON.stringify(firstLoad));
 }
 
 export function loadLocalStorage() {
-    const body = document.querySelector('body');
-    body.onload = function() {
-        console.log('testing on load');
+    let firstLoadInquiry = JSON.parse(localStorage.getItem('firstLoad'));
+    firstLoad = firstLoadInquiry;
+    console.log(firstLoad);
+
+    if (firstLoad == false) {
+        console.log('loading array');
         const userLocalData = JSON.parse(localStorage.getItem('todoArrayStorage'));
         todoArray = userLocalData;
         const idCount = JSON.parse(localStorage.getItem('idCount'));
         id = idCount;
-
-        if (todoArray.length === 0) {
-            return;
-        } else {
-            todoArray.forEach(item => {
-                createDom(item);
-            })
-        }
-    };
+    
+        todoArray.forEach(item => {
+            createDom(item);
+        })
+    } else {
+        return;
+    }
 }
 
 
